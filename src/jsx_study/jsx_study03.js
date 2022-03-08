82e70a2d13522ee6618ea953066c46cb54d12ad1
LeeJin Sim <s2011@e-mirim.hs.kr>
2022-03-08 14:47:11 +0900

    📑 :: 조건부 렌더링

---
+++ src/jsx_study/jsx_study03.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// 인라인 스타일 정의
+// - 스타일 관련 정보를 포함한 객체를 전달하여 인라인 스타일 적용 가능
+// - 단, 객체 속성 이름을 적을때 연산자로 사용되는 하이픈(-)을 포함시킬 수 없으므로, 카멜 표기법을 이용함
+
+
+const h1 = <h1 style={{color: "red", backgroundColor: "lightblue"}}>
+    Hello Style!
+</h1>
+
+ReactDOM.render(h1, document.getElementById('root'));
