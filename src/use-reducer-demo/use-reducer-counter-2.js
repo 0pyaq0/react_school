f04865d2de6ba831d43df2a941789e58dfec58e2
LeeJin Sim <s2011@e-mirim.hs.kr>
2022-04-26 15:07:06 +0900

    ⚡ :: 액션 타입과 관련된 상수 추가

---
+++ src/use-reducer-demo/use-reducer-counter-2.js
@@ -0,0 +1,9 @@
+import React, { useReducer } from "react"
+import ReactDOM from "react-dom"
+
+// 액션 타입과 관련된 상수 추가
+const Action = {
+    INCREASE: 'INCREASE',
+    DECREASE: 'DECREASE',
+    ADD: 'ADD'
+}
