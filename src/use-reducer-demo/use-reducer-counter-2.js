e7c110bfd2d806fd6c1060db4cba8a254e2817b8
LeeJin Sim <s2011@e-mirim.hs.kr>
2022-04-26 15:09:27 +0900

    📝 :: 객체 전달로 수정 - #4

--- src/use-reducer-demo/use-reducer-counter-2.js
+++ src/use-reducer-demo/use-reducer-counter-2.js
@@ -23,3 +23,24 @@ function reducer(state, action) {
             return state
     }
 }
+
+function CounterUsingReducer(props) {
+    const [state, dispatch] = useReducer(reducer, { count: 0 })
+
+    return (
+        <div>
+            <p>count : { state.count }</p>
+            {/* 기존의 문자열을 보내던 방식을 개선하여 객체를 보내도록 수정 */}
+            <button onClick={() => dispatch({ type: Action.INCREASE })}>INCREASE</button>
+            <br />
+            <button onClick={() => dispatch({ type: Action.DECREASE })}>DECREASE</button>
+            <br />
+            {/* 이 경우 특정 숫자(추가 정보)를 더해주는 것이므로 type 정보뿐만 아니라, 더해줄 숫자를 포함할 payload 정보도 같이 보냄 */}
+            <button onClick={() => dispatch({ type: Action.ADD, payload: 10 })}>ADD 10</button>
+            <br />
+            <button onClick={() => dispatch({ type: Action.ADD, payload: -10 })}>SUB 10</button>
+        </div>
+    )
+}
+
+ReactDOM.render(<CounterUsingReducer />, document.getElementById("root"));
