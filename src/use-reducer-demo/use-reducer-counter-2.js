9d246e91ba9fce1a6c446704e5323cdc60704c5e
LeeJin Sim <s2011@e-mirim.hs.kr>
2022-04-26 15:08:28 +0900

    ⚡ :: switch - case 구문으로 변경, ADD 액션 추가 - #3

--- src/use-reducer-demo/use-reducer-counter-2.js
+++ src/use-reducer-demo/use-reducer-counter-2.js
@@ -7,3 +7,19 @@ const Action = {
     DECREASE: 'DECREASE',
     ADD: 'ADD'
 }
+
+function reducer(state, action) {
+    // switch - case 구문으로 변경
+    // type 속성이 포함된 객체를 전달받도록 변경
+    switch(action.type) {
+        case Action.INCREASE:
+            return { count: state.count + 1 }
+        case Action.DECREASE:
+            return { count: state.count - 1 }
+        // 추가 정보값(payload)을 전달받는 ADD 액션 추가
+        case Action.ADD:
+            return { count: state.count + action.payload }
+        default:
+            return state
+    }
+}
